effb60111f4f23f10833f8949875ac0f6664dd36
Henry Wilson <henry.wilson.17@cnu.edu>
2019-07-26 15:47:00 -0400

    Commit before changing jackal base

---
+++ jackal_bringup/scripts/startros.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+#This file is made to start ros automatically at boot
+#Authored by Henry Wilson
+#Replaces the jackal 14.04 start system
+#All variables set up in ~/.bashrc.
+#These include: JACKAL_BB2, JACKAL_LASER, JACKAL_LASER_HOST, etc.
+source /home/$ROSUSER/$ROSENV/devel/setup.bash
+
+roscore > /dev/null &
+sleep 5 &&
+roslaunch jackal_base base.launch > /dev/null &
+sleep 5 &&
+roslaunch jackal_bringup accessories.launch > /dev/null &
+#sleep 5 &&
+#roslaunch jackal_bringup record.launch > /dev/null &
